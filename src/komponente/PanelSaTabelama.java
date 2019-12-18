4ba6281a383d69bc30df3ee73a83e516d4bfdc4a
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2019-12-18 20:20:11 +0100

    #mainframe napravljen je PanelSaTabelama i dodat je u tabbedPane

---
+++ src/komponente/PanelSaTabelama.java
@@ -0,0 +1,56 @@
+package komponente;
+ 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+ 
+import javax.swing.JPanel;
+ 
+public class PanelSaTabelama extends JPanel {
+ 
+    private static final long serialVersionUID = -3777718742688152316L;
+ 
+    private JPanel leviPanel = new JPanel();
+    private JPanel desniPanel = new JPanel();
+    private JPanel gornjiPanel = new JPanel();
+    private JPanel donjiPanel = new JPanel();
+   
+    public PanelSaTabelama() {
+        setLayout(new BorderLayout());
+        add(leviPanel, BorderLayout.WEST);
+        leviPanel.setBackground(Color.LIGHT_GRAY);
+        add(desniPanel, BorderLayout.EAST);
+        desniPanel.setBackground(Color.LIGHT_GRAY);
+        add(gornjiPanel, BorderLayout.NORTH);
+        gornjiPanel.setBackground(Color.LIGHT_GRAY);
+        add(donjiPanel, BorderLayout.SOUTH);
+        donjiPanel.setBackground(Color.LIGHT_GRAY);
+    }
+   
+    public PanelSaTabelama(Component c, int pomeraj) {
+        this();
+        leviPanel.setPreferredSize(new Dimension(pomeraj, pomeraj));
+        desniPanel.setPreferredSize(new Dimension(pomeraj, pomeraj));
+        gornjiPanel.setPreferredSize(new Dimension(pomeraj, pomeraj));
+        donjiPanel.setPreferredSize(new Dimension(pomeraj, pomeraj));
+        add(c, BorderLayout.CENTER);
+       
+       
+    }
+ 
+    @Override
+    public void setBackground(Color bg) {
+        // TODO Auto-generated method stub
+        super.setBackground(bg);
+        if (leviPanel == null || desniPanel == null || donjiPanel == null || gornjiPanel == null)
+            return;
+        leviPanel.setBackground(bg);
+        desniPanel.setBackground(bg);
+        gornjiPanel.setBackground(bg);
+        donjiPanel.setBackground(bg);
+    }
+   
+   
+   
+}
--- src/komponente/TabbedPane.java
+++ src/komponente/TabbedPane.java
@@ -7,10 +7,31 @@ public class TabbedPane extends JTabbedPane {
 	
 	private static final long serialVersionUID = -6641767877803421613L;
 
+	private PanelSaTabelama panelSaTabelomStudent;
+	private PanelSaTabelama panelSaTabelomPredmet;
+	private PanelSaTabelama panelSaTabelomProfesor;
+	private TabelaStudenata tabelaStudenata;
+	private TabelaProfesora tabelaProfesora;
+	private TabelaPredmeta tabelaPredmeta;
+	
 	public TabbedPane() {
-		addTab("Studenti", new ImageIcon("slike/studenti_pane.png"), null, "");
-		addTab("Profesori", new ImageIcon("slike/profesori_pane.png"), null, "");
-		addTab("Predmeti", new ImageIcon("slike/predmeti_pane.png"), null, "");
+		tabelaStudenata = new TabelaStudenata();
+		JScrollPane studenti = new JScrollPane(tabelaStudenata);
+		
+		tabelaProfesora = new TabelaProfesora();
+		JScrollPane profesori = new JScrollPane(tabelaProfesora);
+		
+		tabelaPredmeta=new TabelaPredmeta();
+		JScrollPane predmeti= new JScrollPane(tabelaPredmeta);
+		
+		panelSaTabelomStudent=new PanelSaTabelama(studenti,100);
+		panelSaTabelomProfesor=new PanelSaTabelama(profesori,100);
+		panelSaTabelomPredmet=new PanelSaTabelama(predmeti,100);
+		
+		
+		addTab("Studenti", new ImageIcon("slike/studenti_pane.png"), panelSaTabelomStudent, "");
+		addTab("Profesori", new ImageIcon("slike/profesori_pane.png"), panelSaTabelomProfesor, "");
+		addTab("Predmeti", new ImageIcon("slike/predmeti_pane.png"), panelSaTabelomPredmet, "");
 		
 
 	}
